363e982819907d51d7590d432951a4191a4be688
Viktor Ståhl <viktor.stahl.trash@gmail.com>
2020-05-13 16:43:57 +0200

    Added MQTT example

--- water_sensor/water_sensor.ino
+++ water_sensor/water_sensor.ino
@@ -9,15 +9,38 @@
   Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
 */
 
+/* **** Includes **** */
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+
+/* **** Defines **** */
+
+#define wifi_ssid "Peter"
+#define wifi_password "Lingon53"
+
+#define mqtt_server "192.168.1.19"
+
+#define moisture_topic "sensor/moisture"
+
 #define MOISTURE_SENSOR     (0u)
 #define AIR                 (850)
 #define WATER               (450)
 #define EARTH_SEMI_MOIST    (600)
 
-void readMoisture() {
+/* **** Type Definitions **** */
+
+/* **** Global Variables **** */
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+int readMoisture() {
   int val = analogRead(MOISTURE_SENSOR); //connect sensor to Analog 0
   int moisture = 100 - ((val - WATER) * 100) / (AIR-WATER);
   Serial.printf("Moisture %u%%\n", moisture); // print the value to serial port
+  return moisture;
 }
 
 void readVoltage() { // read internal VCC
@@ -33,20 +56,71 @@ void runTest7() {
   Serial.println(F("What... I'm not asleep?!?"));      // it will never get here
 }
 
+static void setup_wifi() {
+  delay(10);
+  Serial.print("\nConnecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi OK ");
+  Serial.print("=> ESP8266 IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.println();
+}
+
+static void reconnect() {
+
+  while (!client.connected()) {
+    Serial.print("Connecting to MQTT broker ...");
+    if (client.connect("ESP8266Client")) {
+      Serial.println("OK");
+    } else {
+      Serial.print("Error : ");
+      Serial.print(client.state());
+      Serial.println(" Wait 5 secondes before to retry");
+      delay(5000);
+    }
+  }
+}
+
+static void sendMoisture(int moisture) {
+
+  client.publish(moisture_topic, String(moisture).c_str(), true);
+}
+
+static void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println("In callback");
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   Serial.begin(9600);
   Serial.println("\nI am awake!");
+  setup_wifi();           //Connect to Wifi network
+  client.setServer(mqtt_server, 1883);    // Configure MQTT connexion
+  client.setCallback(callback);           // callback function to execute when a MQTT message
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
                                     // but actually the LED is on; this is because
                                     // it is active low on the ESP-01)
   delay(1000);                      // Wait for a second
   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  readMoisture();
+  int moisture = readMoisture();
+  sendMoisture(moisture);
   delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
   // runTest7();
 }
